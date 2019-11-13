3af48f6c94f2584822e775dd4d6dcd99f6f7e684
zfnggg <zarekng@gmail.com>
2019-11-13 16:58:31 +0800

    Update process_registration.php

--- Hotel_Booking_php/process_registration.php
+++ Hotel_Booking_php/process_registration.php
@@ -18,7 +18,7 @@ if (isset($_POST['submit'])) {
     session_start();
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         if (!isset($_POST['_token']) || ($_POST['_token'] != $_SESSION['_token'])){
-            die('Invalid Token');
+            die('Invalid Token or Request Method');
         }
     }
     session_destroy();
