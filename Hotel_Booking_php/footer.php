daa128ef965c5c03d9163c5978ee010980f07f0f
zfnggg <zarekng@gmail.com>
2019-11-13 16:15:50 +0800

    Create footer.php

---
+++ Hotel_Booking_php/footer.php
@@ -0,0 +1,107 @@
+<!-- Footer -->
+<footer class="page-footer font-small stylish-color-dark pt-4">
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+
+        <!-- Grid row -->
+        <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-4 mx-auto">
+
+                <!-- Content -->
+                <h5 class="mt-3 mb-4">D'Hotel</h5>
+                <!-- font-weight-bold text-uppercase -->
+                <p><i class="fas fa-phone iconHotel"></i> +65 6241 0241</p>
+                <p><i class="fas fa-map-marker-alt iconHotel"></i>
+                    <a class="imgLink" href="https://www.google.com/maps/place/Singapore+398741/@1.3118449,103.8637944,14z/data=!3m1!4b1!4m5!3m4!1s0x31da18398e3c39ed:0x56e011efd5cc847f!8m2!3d1.311845!4d103.881304" target="_parent">
+                        23 Lorong 20 Geylang, Singapore 398741
+                    </a></p>
+                <p><i class="fas fa-mail-bulk iconHotel"></i> service@dhotel.sg</p>
+            </div>
+            <!-- Grid column -->
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <!-- Grid column -->
+            <div class="col-md-2 mx-auto">
+
+                <!-- Links -->
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <a class="imgLink" href="./rooms.php" target="_parent">
+
+                            <div class="imgLink">
+                                Rooms
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="imgLink" href="./events.php" target="_parent">
+
+                            <div class="imgLink">
+                                Events
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="imgLink" href="./dining.php" target="_parent">
+
+                            <div class="imgLink">
+                                Dining
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="imgLink" href="./about.php" target="_parent">
+
+                            <div class="imgLink">
+                                About
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </div>
+        <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <hr>
+
+    <ul class="list-unstyled list-inline text-center">
+                <li class="list-inline-item">
+                    <a href="https://www.facebook.com/" aria-label="Facebook" target="_blank" class="g mx-1">
+                        <i class="fab fa-facebook-f" > </i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="https://twitter.com/?lang=en" aria-label="Twitter" target="_blank"  class="btn-floating btn-tw mx-1">
+                        <i class="fab fa-twitter"> </i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="" target="_blank"   aria-label="Google" class="btn-floating btn-gplus mx-1">
+                        <i class="fab fa-google-plus-g"> </i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="https://www.instagram.com/" aria-label="Instagram" target="_blank"  class="btn-floating btn-in mx-1">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="https://www.linkedin.com/login" aria-label="Linkedin" target="_blank"  class="btn-floating btn-li mx-1">
+                        <i class="fab fa-linkedin-in"> </i>
+                    </a>
+                </li>
+						
+            </ul>
+</footer>
+<!-- Footer -->
