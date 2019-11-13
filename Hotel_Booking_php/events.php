2e575e02a34b40990e0c5b7048b41a7449821810
zfnggg <zarekng@gmail.com>
2019-11-13 16:15:45 +0800

    Create events.php

---
+++ Hotel_Booking_php/events.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="en">
+    <head>
+        <title>D'Hotel</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">        
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="shortcut icon" type="image/icon" href="./img/favicon.ico"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Main CSS Style Sheet-->
+        <link href="css/main.css" rel = "stylesheet" />        
+        <!-- Zheng Feng CSS -->
+        <!-- Events CSS Style Sheet-->
+        <link href="css/events.css" rel = "stylesheet" /> 
+        <!-- FAQ CSS Style Sheet-->
+        <link href="css/faq.css" rel = "stylesheet" /> 
+        <!-- Dining CSS Style Sheet-->
+        <link href="css/dining.css" rel = "stylesheet" />        
+        <!-- Font Awesome Icons -->
+        <script src='https://kit.fontawesome.com/a076d05399.js'></script>      
+        <!-- Own Javascript -->
+        <script defer src="js/main.js"></script> 
+    </head>
+
+    <body>
+        <!-- Start of Navigation Bar -->
+        <div id="nav-placeholder">
+            <script>
+                $(function () {
+                    $("#nav-placeholder").load("navbaruser.php");
+                });
+            </script>
+        </div>
+        <!--end of Navigation bar-->
+
+        <div class="container-fluid">
+            <div class="row">
+                <header class="col">
+                    <div class="article">
+                        <h3 class="display-4 text-center">Events</h3>
+                    </div>
+                </header>
+            </div>
+        </div>
+
+        <div class="eventroom-image">
+            <div class="eventroom-text">
+                <h1>The Foyer</h1>
+                <p>No strangers to hospitality, you can count
+                    on the D' Hotel team to assist in planning and delivering
+                    exceptional experiences for you and your guests.</p>
+                <p><em>Full buyouts available upon request.</em></p>
+            </div>
+        </div>
+
+        <div class="ballroom-image">
+            <div class="ballroom-text">
+                <h1>The Hall</h1>
+                <p>Exquisite interior space designed to better suit
+                    your special day. Perfect for Weddings, Dance-and-Dinners, Ceremonies.
+                </p>
+                <p><em>"The perfect day calls for the perfect room."</em></p>
+            </div>
+        </div>
+
+        <!--Footer-->
+        <div id="footer-placeholder">
+            <script>
+                $(function () {
+                    $("#footer-placeholder").load("footer.php");
+                });
+            </script>
+        </div>
+        <!--end of Footer-->
+    </body>
+</html>
