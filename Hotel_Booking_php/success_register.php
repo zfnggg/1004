34a7a8629cdf75a7c9823cd2168aab45e5f78fe6
jiaengoh <hihihi_97@hotmail.com>
2019-11-13 21:59:06 +0800

    Create success_register.php

---
+++ Hotel_Booking_php/success_register.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="en">
+    <head>
+        <title>D'Hotel</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>-->
+        <link href="css/main.css" rel="stylesheet" />
+        <link href="css/registration.css" rel="stylesheet" />
+        <!-- Own CSS Style Sheet-->
+        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+        <!-- Font Awesome Icons -->
+        <script defer src="js/main.js"></script>
+        <!-- Own Javascript -->
+    </head>
+
+    <body>
+        <!-- Start of Navigation Bar -->
+        <div id="nav-placeholder">
+            <script>
+                $(function () {
+                    $("#nav-placeholder").load("navbaruser.php");
+                });
+            </script>
+        </div>
+        <!--end of Navigation bar-->
+
+        <section>
+            <div class="jumbotron text-center bg-light">
+                <h2>Your account has been successfully registered</h2>
+                <p>Click here to <a href="./login.php">sign in</a></p>
+            </div>
+
+            <img src="img/bookroom1.jpg" class="mx-auto d-block img-fluid shrinkImageFam">
+
+            <!--Footer-->
+            <div id="footer-placeholder">
+                <script>
+                    $(function () {
+                        $("#footer-placeholder").load("footer.php");
+                    });
+                </script>
+            </div>
+            <!--end of Footer-->
+    </body>
+</html>
