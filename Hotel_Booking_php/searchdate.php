07dea8c895c0c38196827581c62e210a9936b157
jiaengoh <hihihi_97@hotmail.com>
2019-11-14 19:52:21 +0800

    Update searchdate.php

--- Hotel_Booking_php/searchdate.php
+++ Hotel_Booking_php/searchdate.php
@@ -24,16 +24,16 @@ if (!isset($_SERVER['HTTP_REFERER'])) {
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <!-- Main CSS Style Sheet-->
-        <link href="css/main.css" rel = "stylesheet" /> 
+        <link href="css/main.css" rel = "stylesheet" />
         <!-- Zheng Feng CSS -->
         <!-- Events CSS Style Sheet-->
-        <link href="css/events.css" rel = "stylesheet" /> 
+        <link href="css/events.css" rel = "stylesheet" />
         <!-- FAQ CSS Style Sheet-->
-        <link href="css/faq.css" rel = "stylesheet" /> 
+        <link href="css/faq.css" rel = "stylesheet" />
         <!-- Dining CSS Style Sheet-->
-        <link href="css/dining.css" rel = "stylesheet" /> 
+        <link href="css/dining.css" rel = "stylesheet" />
         <!-- Font Awesome Icons -->
-        <script src='https://kit.fontawesome.com/a076d05399.js'></script> 
+        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
         <!-- Own Javascript -->
         <script defer src="js/main.js"></script>
     </head>
@@ -41,15 +41,15 @@ if (!isset($_SERVER['HTTP_REFERER'])) {
     <main>
         <div class="jumbotron text-center">
             <h1>Search Booking Date</h1>
-            <a href="customerprofile.php" title="manage">Customer</a> | 
-            <a href="booking.php" title="manage">Booking</a> | 
+            <a href="customerprofile.php" title="manage">Customer</a> |
+            <a href="booking.php" title="manage">Booking</a> |
             <a href="bookingsummary.php" title="manage">Booking Summary </a>
         </div>
 
-        <div class="container-fluid text-center">    
+        <div class="container-fluid text-center">
             <div class="row content">
                 <div class="col-sm-2"></div>
-                <div class="col-sm-8 text-left">       
+                <div class="col-sm-8 text-left">
                     <form name="search" method="post" action="searchdate.php">
                         <label for="searchdate">Search Booking by Date:
                             <input type="date" id="searchdate" name="searchdate" required>
@@ -126,3 +126,4 @@ if (!isset($_SERVER['HTTP_REFERER'])) {
     include "./footer.php";
     ?>
 </html>
+
