72e798efb072393ab7a50120a5c695f2b453129b
jiaengoh <hihihi_97@hotmail.com>
2019-11-17 16:43:37 +0800

    Update editmyprofile.php

--- Hotel_Booking_php/editmyprofile.php
+++ Hotel_Booking_php/editmyprofile.php
@@ -30,17 +30,10 @@ if (!isset($_SERVER['HTTP_REFERER'])) {
     <script defer src="js/main.js"></script>
     <!-- Own Javascript -->
 </head>
+<?php
+include "./navbaruser.php";
 
-<!-- Start of Navigation Bar -->
-<div id="nav-placeholder">
-    <script>
-        $(function() {
-            $("#nav-placeholder").load("navbaruser.php");
-        });
-
-    </script>
-</div>
-<!--end of Navigation bar-->
+?>
 
 <body>
     <div class="jumbotron text-center">
