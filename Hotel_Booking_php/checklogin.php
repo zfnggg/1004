db97abf4b3155daf195749486f106be43393daea
haziqmofe <55946335+haziqmofe@users.noreply.github.com>
2019-11-17 17:51:36 +0800

    Update checklogin.php

--- Hotel_Booking_php/checklogin.php
+++ Hotel_Booking_php/checklogin.php
@@ -1,42 +1,43 @@
 <!doctype html>
 
-<?php
-include "./navbaruser.php";
-?>
-
 <html lang="en-US">
-    <head>
-        <title>D'Hotel</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link rel="shortcut icon" type="image/icon" href="./img/favicon.ico"/>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>-->
-        <link href="css/main.css" rel="stylesheet" />
-        <!-- Own CSS Style Sheet-->
-        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-        <!-- Font Awesome Icons -->
-        <script defer src="js/main.js"></script>
-
-        <!-- Own Javascript -->
-    </head>
 
 <head>
-    <meta charset="utf-8">
-    <title>Check Login</title>
+    <title>D'Hotel</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" type="image/icon" href="./img/favicon.ico" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>-->
+    <link href="css/main.css" rel="stylesheet" />
+    <!-- Own CSS Style Sheet-->
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <!-- Font Awesome Icons -->
+    <script defer src="js/main.js"></script>
+
+    <!-- Own Javascript -->
 </head>
 
-<body>
-
+<header>
     <?php
+    include "./navbaruser.php";
+    ?>
+
+    <meta charset="utf-8">
+    <title>Check Login</title>
+</header>
+
+<body>
+    <main>
+        <?php
         //session_start();
         //Just for MAC, for windows need to change accordingly 
         //require_once('/Applications/XAMPP/xamppfiles/protected/config.php');
         //For windows
         require_once('../protected/config.php');
-        
+
         $email = $errorMsg = "";
         $success = true;
         function sanitize_input($data)
@@ -46,27 +47,20 @@ include "./navbaruser.php";
             $data = htmlspecialchars($data);
             return $data;
         }
-        
-        if(empty($_POST["username"]))
-        {
+
+        if (empty($_POST["username"])) {
             $errorMsg .= "Username is required.<br>";
             $success = false;
-        }
-        else
-        {
+        } else {
             $username = sanitize_input($_POST["username"]);
-           
         }
-        if(empty($_POST["password"]))
-        {
+        if (empty($_POST["password"])) {
             $errorMsg .= "Password is required.<br>";
             $success = false;
+        } else {
+            $p = sanitize_input($_POST["password"]);
+            $p = md5($p);
         }
-         else
-         {
-             $p = sanitize_input($_POST["password"]);
-             $p= md5($p);
-         }
         $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
         if (isset($_POST["submit"])) {
             $c = $_POST['captcha'];
@@ -100,7 +94,7 @@ include "./navbaruser.php";
                     echo "<div class=row>";
                     echo "<div class='col-sm-3'></div>";
                     echo "<div class='col-sm-6'>";
-                    echo "<h1>Username not found or password doesn't match...<h1>";                    
+                    echo "<h1>Username not found or password doesn't match...<h1>";
                     echo "</div>";
                     echo "<div class='col-sm-3'></div>";
                     echo "<br>";
@@ -119,9 +113,7 @@ include "./navbaruser.php";
 
                 $sql->close();
                 mysqli_close($conn);
-            } 
-            
-            else {
+            } else {
                 echo "<div class=row>";
                 echo "<div class='col-sm-3'></div>";
                 echo "<div class='col-sm-6'>";
@@ -142,12 +134,14 @@ include "./navbaruser.php";
                 echo "<hr>";
                 session_destroy();
             }
-            
-        }?>
-    </body>
-    
-       <?php
-        include "./footer.php";
-        ?>
+        } ?>
+    </main>
+
+
+    <?php
+    include "./footer.php";
+    ?>
+
+</body>
 
 </html>
