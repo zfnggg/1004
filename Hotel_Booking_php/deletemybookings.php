c1ec8d666a804ee8bf233c512305a6ea3ad96350
jiaengoh <hihihi_97@hotmail.com>
2019-11-18 19:47:27 +0800

    Create deletemybookings.php

---
+++ Hotel_Booking_php/deletemybookings.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!isset($_SERVER['HTTP_REFERER'])) {
+// redirect them to your desired location
+    header('location:login.php');
+
+    exit;
+}
+?>
+<?php
+
+
+require_once('../protected/config.php');
+$conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+
+$cidToDelete = $_POST["cid"];
+$sql = $conn->prepare("delete from booking where bookingID=?");
+$sql->bind_param("i", $cidToDelete);
+$sql->execute();
+$result = $sql->get_result();
+$sql->close();
+ mysqli_close($conn);
+//$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+header("Location:reservation.php?id=$cidToDelete");
+?>
