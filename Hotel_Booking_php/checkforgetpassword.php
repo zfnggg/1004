09cd2cfb10c0a0bfac61710c1f052345ed10e4e8
haziqmofe <55946335+haziqmofe@users.noreply.github.com>
2019-11-27 13:31:34 +0800

    Update checkforgetpassword.php

--- Hotel_Booking_php/checkforgetpassword.php
+++ Hotel_Booking_php/checkforgetpassword.php
@@ -1,9 +1,9 @@
 <!doctype html>
-
-<?php
-include "./navbaruser.php";
-?>
-
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html lang="en-US">
 
 <head>
@@ -29,96 +29,85 @@ include "./navbaruser.php";
     <script defer src="js/main.js"></script>
 </head>
 
-<head>
-    <meta charset="utf-8">
-    <title>Check Login</title>
-</head>
-
-
-
-<?php
-
-// Constants for accessing our DB: 
-define("DBHOST", "161.117.122.252");
-define("DBNAME", "p1_4");
-define("DBUSER", "p1_4");
-define("DBPASS", "5xLMQfLGsc"); 
-$errorMsg = "";
-$email = sanitize_input($_POST["email"]);
-$success = true;
-
-
-function sanitize_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-
-if (empty($_POST["email"])) {
-    $errorMsg .= "Email is required.<br>";
-    $success = false;
-} 
-
-else 
-{
-    $email = sanitize_input($_POST["email"]);
-    //To check if the email address is well formed 
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errorMsg .= "Invalid email format.";
-        $success = false;
-    }
-
-    else
-    {
-        //Create connection
-        $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
-
-        //Check connection 
-        if ($conn->connect_error) 
-        {
-            $errorMsg = "Connection failed: " . $conn->connect_error;
-            $success = false;
-        } 
-        
-        else 
-        {
-            $sql = "SELECT * FROM users WHERE ";
-            $sql .= "email = '$email'";
-
-            //Execute the query 
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                //Note that email field is unique, so should only have 
-                //one row in the result set. 
-                $row = $result->fetch_assoc();
-                $email = $row["email"];
-
-                $_SESSION['email'] = $email;
-
-                //        echo"<h2>Login Successfully</h2>";
-                //        echo"<p>Welcome Back,$fname <br>" ;
-                //        
-                //        echo"<form action ='index.php'>";
-                //        echo "<input type='submit' value='Return to home'>";
-                //        echo"</form>";
-            } 
-            
-            else 
-            {
-                $success = false;
-                $errorMsg .= "Your email does not exist.";
-            }
-            $result->free_result();
-        }
-        $conn->close();
-        
-    }
-}
-?>
-
 <body>
+    <header>
+        <?php
+        include "./navbaruser.php";
+        ?>
+        <title>Check Login</title>
+    </header>
+
+
+
+    <?php
+
+    // Constants for accessing our DB: 
+    define("DBHOST", "161.117.122.252");
+    define("DBNAME", "p1_4");
+    define("DBUSER", "p1_4");
+    define("DBPASS", "5xLMQfLGsc");
+    $errorMsg = "";
+    $email = sanitize_input($_POST["email"]);
+    $success = true;
+
+
+    function sanitize_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    if (empty($_POST["email"])) {
+        $errorMsg .= "Email is required.<br>";
+        $success = false;
+    } else {
+        $email = sanitize_input($_POST["email"]);
+        //To check if the email address is well formed 
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errorMsg .= "Invalid email format.";
+            $success = false;
+        } else {
+            //Create connection
+            $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+
+            //Check connection 
+            if ($conn->connect_error) {
+                $errorMsg = "Connection failed: " . $conn->connect_error;
+                $success = false;
+            } else {
+                $sql = "SELECT * FROM users WHERE ";
+                $sql .= "email = '$email'";
+
+                //Execute the query 
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    //Note that email field is unique, so should only have 
+                    //one row in the result set. 
+                    $row = $result->fetch_assoc();
+                    $email = $row["email"];
+
+                    $_SESSION['email'] = $email;
+
+                    //        echo"<h2>Login Successfully</h2>";
+                    //        echo"<p>Welcome Back,$fname <br>" ;
+                    //        
+                    //        echo"<form action ='index.php'>";
+                    //        echo "<input type='submit' value='Return to home'>";
+                    //        echo"</form>";
+                } else {
+                    $success = false;
+                    $errorMsg .= "Your email does not exist.";
+                }
+                $result->free_result();
+            }
+            $conn->close();
+        }
+    }
+    ?>
+
+
     <div class="container">
         <p><?php
 
