b45fccaa22c4646f239e32cf43c4ea6529357fc7
jiaengoh <hihihi_97@hotmail.com>
2019-11-28 23:56:42 +0800

    Update booking.php

--- Hotel_Booking_php/booking.php
+++ Hotel_Booking_php/booking.php
@@ -125,8 +125,8 @@ if (!isset($_SERVER['HTTP_REFERER'])) {
                                     </td>
 
                                     <td>
-                                        <!-- action="editbookingrecord.php" -->
-                                        <form name="booking" method="post" novalidate>
+                                        
+                                        <form name="booking" method="post" action="editbookingrecord.php" novalidate>
                                             <input name="cid" type="hidden" value="<?php echo $data['bookingID'] ?>">
                                             <br />
                                             <label>
